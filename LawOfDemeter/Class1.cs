d398023bc2da61fb08f5aabf58d1558cf65c5967
Kim Reneil Paule <kim.paule@mvp.net.ph>
2023-09-18 13:35:20 +0800

    Add project files.

---
+++ LawOfDemeter/Class1.cs
@@ -0,0 +1,61 @@
+﻿namespace LawOfDemeter1
+{
+    public class Class1
+    {
+
+    }
+
+    public class Wallet
+    {
+        public decimal MoneyAmount { get; private set; }
+
+        public Wallet(decimal moneyAmount)
+        {
+            MoneyAmount = moneyAmount;
+        }
+
+        public void AddMoney(decimal amount)
+        {
+            MoneyAmount += amount;
+        }
+
+        public void WithdrawMoney(decimal amount)
+        {
+            MoneyAmount -= amount;
+        }
+    }
+
+    public class Customer
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public Wallet Wallet { get; }
+
+        public Customer(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Wallet = new Wallet(1000m);
+        }
+    }
+
+    public class PaperBoy
+    {
+        public void DeliverMagazine(decimal costOfMagazine, Customer customer)
+        {
+            Wallet w = customer.Wallet;
+
+            if (w.MoneyAmount > costOfMagazine)
+            {
+                w.WithdrawMoney(costOfMagazine);
+            }
+            else
+            {
+                //
+            }
+        }
+    }
+
+
+
+}
---
+++ LawOfDemeter/Class2.cs
@@ -0,0 +1,71 @@
+﻿namespace LawOfDemeter2
+{
+    public class Class2
+    {
+
+    }
+
+    public class Wallet
+    {
+        public decimal MoneyAmount { get; private set; }
+
+        public Wallet(decimal moneyAmount)
+        {
+            MoneyAmount = moneyAmount;
+        }
+
+        public void AddMoney(decimal amount)
+        {
+            MoneyAmount += amount;
+        }
+
+        public void WithdrawMoney(decimal amount)
+        {
+            MoneyAmount -= amount;
+        }
+    }
+
+    public class Customer
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        private readonly Wallet wallet;
+
+        public Customer(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            wallet = new Wallet(1000m);
+        }
+
+        public decimal GetPayment(decimal amount)
+        {
+            if (wallet.MoneyAmount >= amount)
+            {
+                wallet.WithdrawMoney(amount);
+                return amount;
+            }
+            return 0;
+        }
+    }
+
+    public class PaperBoy
+    {
+        public void DeliverMagazine(decimal costOfMagazine, Customer customer)
+        {
+            decimal payment = customer.GetPayment(costOfMagazine);
+
+            if (payment == costOfMagazine)
+            {
+                //
+            }
+            else
+            {
+                //
+            }
+        }
+    }
+
+
+
+}
